b7f72a0ebfff1df4eed36d57641cb1000f8d2215
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-27 15:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays(){
   return 'Happy holidays!'
 }
 function happyHolidaysTo(name) {
-  console.log('Happy holidays, ${name}!')
+  console.log(`Happy holidays, ${name}!`)
 }
 function happyHolidayTo(holiday, name){
-  return 'Happy ${holiday}, ${name}!'
+  return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(days, holiday){
-  return 'It\'s ${days} days until ${holiday}!'
+  return `It\'s ${days} days until ${holiday}!`
 }
